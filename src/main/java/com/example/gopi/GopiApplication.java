2afe84022ec86c87e2b75b1829913774e432fad3
pavankatari143 <pavan.k@suntecnoservices.com>
2020-03-09 01:23:55 -0700

    this is gopi

---
+++ src/main/java/com/example/gopi/GopiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gopi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GopiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GopiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gopi/GopiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gopi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GopiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
